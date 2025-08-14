2a40a20d6bb72bf3933de0b5a918d63de6111019
xay2001 <1602357810@qq.com>
2025-08-14 20:35:45 +0800

    [ADD]: Add weight pruning scripts for the CIFAR-10 dataset, including methods based on weight magnitude and random pruning.

---
+++ scripts/weight_prune/prune_ddpm_weight_magnitude_cifar10.sh
@@ -0,0 +1,8 @@
+python ddpm_weight_prune.py \
+--dataset cifar10 \
+--model_path pretrained/ddpm_ema_cifar10 \
+--save_path run/pruned/weight_magnitude/ddpm_cifar10_weight_pruned \
+--pruning_ratio $1 \
+--batch_size 128 \
+--pruner magnitude \
+--device cuda:1 \
